a6cb798a620cee05ce6d54ac16f2558845d904da
Godspeed-exe <michiel.bellen@gmail.com>
2025-06-30 12:26:23 +0200

    add creation of random Fernet key

--- make_env.sh
+++ make_env.sh
@@ -9,12 +9,14 @@ read POSTGRES_PASSWORD
 echo "Enter your Blockfrost API key:"
 read BLOCKFROST_PROJECT_ID
 
-echo "Enter the encryption key that will be used to encrypt your wallets:"
-read WALLET_ENCRYPTION_KEY
+FERNET_KEY=$(openssl rand -base64 32)
+
+echo "Generated Fernet encryption key:"
+echo "$FERNET_KEY"
 
 echo "POSTGRES_USER="$POSTGRES_USER >> .env
 echo "POSTGRES_PASSWORD="$POSTGRES_PASSWORD >> .env
 echo "BLOCKFROST_PROJECT_ID="$BLOCKFROST_PROJECT_ID >> .env
-echo "WALLET_ENCRYPTION_KEY="$WALLET_ENCRYPTION_KEY >> .env
+echo "WALLET_ENCRYPTION_KEY="$FERNET_KEY >> .env
 
 
