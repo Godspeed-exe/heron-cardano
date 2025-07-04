fd42eb8ea5b7f732b581f384982d96e158cfc7b7
Michiel <michiel.bellengmail.com>
2025-07-04 14:57:14 +0200

    add link back to Hubspot

--- scripts/generate_docs.py
+++ scripts/generate_docs.py
@@ -16,12 +16,38 @@ with open("docs/index.html", "w") as f:
     <link href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" rel="stylesheet" />
             
     <style>
+
       .try-out, .execute-wrapper, .btn.try-out__btn {
         display: none !important;
       }
+
+      body {
+        margin: 0;
+        background: #f5f5f5;
+      }
+      .topbar-wrapper {
+        background: #2c3e50;
+      }
+      .github-link {
+        padding: 10px;
+        background-color: #eaf2ff;
+        font-family: sans-serif;
+        font-size: 14px;
+        text-align: center;
+      }
+      .github-link a {
+        color: #0366d6;
+        text-decoration: none;
+        font-weight: bold;
+      }
+    
     </style>
   </head>
   <body>
+    <div class="github-link">
+      👉 View this project on <a href="https://github.com/Godspeed-exe/heron-cardano" target="_blank">GitHub</a>
+    </div>
+            
     <div id="swagger-ui"></div>
     <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
     <script>
